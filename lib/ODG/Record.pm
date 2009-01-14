78ac713801a4e460a6a5087fa71c0fb977d34994
Christopher Brown <ctbrown@cpan.org>
2009-01-14 09:52:02 +0000

    Import of CTBROWN/ODG-Record-0.30 from CPAN.
    gitpan-cpan-distribution: ODG-Record
    gitpan-cpan-version:      0.30
    gitpan-cpan-path:         CTBROWN/ODG-Record-0.30.tar.gz
    gitpan-cpan-author:       CTBROWN
    gitpan-cpan-maturity:     released

--- lib/ODG/Record.pm
+++ lib/ODG/Record.pm
@@ -1,99 +1,98 @@
-our $VERSION = '0.28';
-
-
 # ----------------------------------------------------------------------
 # CLASS: ODG::Record
-#
 # ----------------------------------------------------------------------
 package ODG::Record;
+use 5.008;
 use Moose;
-use ODG::Layout;
+	with 'MooseX::Meta::Attribute::Index';
 
-  # has _type_     => ( is => 'rw', isa => 'Str' );
+our $VERSION = '0.30';
 
-  # Attribute _metadata_
-	has _layout_ => ( 
-        is          => 'ro'          , 
-        isa         => 'ODG::Layout' , 
-        required    => 1             ,
-    );
-
-  # Attribute: _data
-	has _data_   => ( 
-	    isa         => 'ArrayRef'     , 
-	    predicate   => '_has_data_'   ,
-        default     => sub { [] }     ,
+  # We do not install any accessors since we want to make this 
+  # lvalue ready,
+	has _data	=> ( 
+	    isa          => 'ArrayRef'    , 
+	    # predicate    => '_has_data'   ,
+        default      => sub { [] }    , 
+		documentaion => 'slot for holding data' ,
 	);
 
 
-# _data_
-#   lvalue 
-sub _data_ :lvalue {
+sub _has_data {
+	scalar( @{ $_[0]->_data } ) > 1;
+}
 
-    $_[0]->{_data_} = $_[1] if ( $_[1] );
-    $_[0]->{_data_};
+
+# _data
+#   lvalue accessor to the data slot.
+sub _data :lvalue {
+
+    $_[0]->{_data} = $_[1] if ( $_[1] );
+    $_[0]->{_data};
 
 }
 
 
-sub _metadata_ {
- 
-    $_[0]->_layout_->_metadata_;
-
-}
-
-  # Should setter (writer) be defined?
-    # has _is_rw_  => ( is => 'rw', isa => 'Bool', default => 1 );
-
-  # Should lvalue accessors be generated
-    # has _has_lvalue_ => ( is => 'rw', isa => 'Bool', default => 1 );
-
-
-
-    
 # -----------------------------------------------------------------------
 # FIELD ACCESSORS:
-#    Create FIELD ACCESSORS for each _layout_ field.
+#    Create FIELD ACCESSORS for each field.
 #
 #  We want to install instance methods and not class methods.  The class
 #  method would cause these methods to be available to subsequent classes.
 #  This would be undesirable because there might be two ODG::Record objects
-#  in the same program.
-#
-#  The solution is to create the methods in a reblessed anonymous class.
 #  Thanks to Paul Driver     
+# 
 # -----------------------------------------------------------------------
 sub BUILD {
 
+	use Data::Dumper;
     my ( $self, @args ) = @_;
+	my $meta = $self->meta;
+	my $index;
 
-  # ---------------------------------------------------------------
-  # Foreach field identified in the metadata install a new lvalue 
-  # accesor method.
-  #
-  # Note:
-  #   - the offset '-1' to correct for Perl's indexing beginning
-  #     at 1    
-  # ---------------------------------------------------------------
-    my $methods = {};
-    METHODS: foreach my $field ( @{ $self->_layout_->_metadata_ } ) {
-    
-      # add_method:
-        $self->meta->add_method( $field->name , sub :lvalue { 
+	foreach my $field ( keys %{ $meta->get_attribute_map } ) {
 
-              # Update the old value if used  second argument is passed 
-                $self->{_data_}->[ $field->{ index } ] = $_[1] if ( $_[1] ); 
+		my $attr = $meta->get_attribute( $field );
+
+	  # Check to see if each index is used only once 
+		if ( $attr->can('index') ) {
+
+			$index->{ $attr->index }++ ;
+			confess( "Two fields share index: " . $attr->index ."\n" ) 
+			  if ( $index->{ $attr->index } > 1 );
+
+			if ( 
+				$attr->_is_metadata and
+				$attr->_is_metadata eq 'rw' 
+			) {
+			
+			# Installs lvalue accessor
+				$meta->add_method( 
+					$field , 
+					sub :lvalue { 
+
+					  # Update the old value if used  second argument is passed 
+						$self->{_data}->[ $attr->index ] = $_[1] if ( $_[1] ); 
         
-              # LVALUE return 
-                $self->{_data_}->[ $field->{ index } ] ;
-            }
-        )
+					  # LVALUE return 
+						$self->{_data}->[ $attr->index ] ;
+					} 
+				);
 
-    } # END LOOP: METHODS 
+			} else {
+			
+				$meta->add_method( 
+					$field, 
+					sub { $self->{_data}->[ $attr->index ] } 
+				);
 
-    return( $self );
+			}
 
-} # END SUB: BUILD
+		}
+
+	}
+
+}
 
 
 1;
@@ -102,179 +101,93 @@ __END__
 
 =head1 NAME
 
-ODG::Record - Perl extension for efficient and simple manipulation of row based records. 
+ODG::Record - Perl extension for efficient and simple manipulation of row-based records. 
+
+=head1 VERSION
+
+0.30
 
 =head1 SYNOPSIS
 
-  use ODG::Record;
-  my $record = ODG::Record->new( 
-    { 
-        _data_      => [1..25] ,
-        _layout_  => ODG::Layout->new( .. )
-    } 
-  );
+	package  MyClass;
 
-  # Data can then be accessed with the _data_ l-value accessor
-  $record->_data_ = [ 26..50 ] ;   
+		use Moose;
+			extends 'ODG::Record';
+			
+
+		has first_name => (
+			is 			=> 'rw' ,
+			isa			=> 'Str',
+			index		=> 0 ,           # Denotes ArrayRef position for attribute
+			traits		=> [ 'Index' ] , # Required trait
+		);
+
+		... 
+
+
+	package main;
+		use ODG::Record;
+
+		my $record = ODG::Record->new( _data => [ 'frank', 'n', 'stein' ] ) );
+
+      # Indexed attributes are stored in the ArrayRef and can be accessed
+      # as any other attribute except that 
+
+		print $record->first_name; 		# frank
+
+		
+	  # Data can retrieved by using the _data attribute.
+		$record->_data = [ 'char', 'lee', 'brown' ] ;   
+
+		$record->first_name; # char
+
+
+	  # L-value attributes can also be defined.
+
+		$record->first_name = 'Robert'  
 
 
 =head1 DESCRIPTION
 
-ODG::Record is an extensible class for efficiently and simply working
-with row based records.  In short, this module provides two functions:
-lvalue accesors to the record (for simplicity) and manipultation data
-using an ArrayRefs for (for Performance).
+THIS VERSION BREAKS BACKWARD WITH PREVIOUS VERSIONS.  THE API IS SIMPLIER.
 
-lvalue accessors allow you to be terse with your code.
+ODG::Record is an extensible L<Moose>-based class for efficiently and 
+simply working with row based records.  ArrayRefs are much faster than 
+HashRefs especially for long arrays. This module circumvents the Mooses' 
+HashRef based objects and places designated attributes in the _data slot, 
+an ArrayRef.  This allows for construction of more efficient row-based
+processing but retains the Moose flavor.
+
+To work with a new record, simply change the reference of the _data slot.
+
+	$record->_data( [ 'Some', 'Array', 'Ref' ] );
+
+Since the emphasis is on speed and generally connection to tightly typed
+systems such as Databases, we break the Moose encapsulation and install
+our own type-checking free accessors.  Type checking is left up to the 
+user.  Since we have eliminated the type-checking, we have also added 
+another bit of magic, L-based accessors.  So rather than coding:
+
+	$record->first_name( "Frank" );
+
+You can use the much more natural appearing:
 
     $record->first_name = "Frank" 
 
+This only works for attributes that are placed in the ArrayRef.   Other
+attributes have normal Moose behavior.
 
 
-head1 DETAILS
+=head1 DETAILS
 
-Data and layout information are separate concerns existing in seperate 
-slots (_data_ and _layout_ ) within the ODG::Record object.
+Placing the record data in an ArrayRef _data slot allows far greater 
+efficient when processing row-based records.  Rather than creating a
+new object for each row, ODG::Record recycles the object and only swaps
+the reference of the _data. Since data is stored as an ArrayRef, this 
+is a huge performance win.
 
-Since the _layout_ (i.e. metadata) does not change between records, 
-separating the _data_ from _layout_ allows for greater efficient 
-when processing row based records via object recycling.  Rather than
-creating a new object for each record, the new data is
-placed in the _data_ slot of an existing ODG::Record object.  Since
-data is stored as an ArrayRef, this is a huge performance win.
-
-ODG::Records require an L<ODG::Layout> object ( a _data_ ArrayRef is
-optional ) for instantiation.  During object construction, name-based 
-accessors are built for each recod field. By default, the accessors 
-permit lvalue assignment. 
-
-
-=head1 Object Model
-
-  ODG::Record
-    |
-    |- slot: _data_
-    |
-    |- slot: _layout_ (container for ODG::Metadata objects )
-    |    |
-    |    |- slot: _metadata_
-    |              
-    |               
-    |- slot: _metadata_ ( ref to ODG::Record::_layout_::_metadata_
-
-
-=head1 DISCUSSION
-
-=head2 Object Recycling
-
-This module is designed for efficient streaming, i.e. accessing
-only one record at a time.  By repopulating the data slot, i.e. 
-recycling the record object, we do not incur the expensive of 
-object instantiation for each record.  A huge win.
-
-The downside is that the checking / validation in new object creation
-might be lost. This may or not be acceptable depending on the situation.
-Generally, when the records are well-defined and well-maintained,  
-i.e. from a database, this is not an issue.  The data from one record 
-to the next is fairly consistent.  
-
-Encapsulation may also be lost.  Again, whether this is acceptable
-depends on the situation.  There are several methods of object 
-recycling ( as opposed to creating a new object).  They are: 
-
-* using a standard accessor, 
-* using an lvalue accessor (not officially supported
-  by Moose -and- may break encapsulation.  ( Since this is Moose,  
-  other Moose techniques can be used for validation, e.g. after method)
-* direct access (breaks encapsulation).    
-
-
-=head2 _data_ assignment performance
-
-Here is a comparison of methods for placing new data in the record
-object on an Intel(R) Xeon(TM) CPU 3.06GHz processor:
-
-  Data has 5 elements: 1..5
-                      Rate    new object moose accessor lvalue moose direct access
-  new object        1268/s            --           -99%        -100%         -100%
-  moose accessor  222222/s        17428%             --         -56%          -78%
-  lvalue moose    500000/s        39337%           125%           --          -50%
-  direct access  1000000/s        78775%           350%         100%            --
-
-
-  Data has 25 elements: 1..25
-                     Rate    new object moose accessor  lvalue moose direct access
-  new object       1243/s            --           -99%         -100%         -100%
-  moose accessor 166667/s        13308%             --          -37%          -54%
-  lvalue moose   266667/s        21353%            60%            --          -27%
-  direct access  363636/s        29155%           118%           36%            --
-  
-  
-  new object      : ODG::Record->new( { _data_ => qw( [ 1..5] ) } );
-  moose accessor  : $record->data( [ 1..5 ] ) 
-  lvalue moose    : $record->data = [ 1..5 ] 
-  direct access   : $record->_data_ = [ 1.5 ]
- 
-In real situations, there is probably not much of a difference between
-the last three techniques, other bottleneck are likely to occur in the
-the code such as I/O ability to return > 100k/s.
- 
-Note to self: What is the comparison to fully encapsulate inside-out objects.  Since
-inside out objects uses references, we would expect them to behave 
-similar to the moose accessors.
-  
-
-=head2 RecordSet
-
-This idea can be extended to a RecordSet where multiple records 
-are placed in the data slot.  This may be advantageous when 
-batching is more appropriate.  Some reasons for this might be 
-related to:
-
-    * I/O constraints, especially latency provide for more 
-      efficient batch processing 
-
-    * Processing requires batch methods, look-backs e.g.  
-
-    * Processing benefits from batch methods. i.e. records
-      are sorted in order and one event needs to be triggered
-      for all records of one type.  
- 
-An example of a RecordSet is demonstrated at:
-
-    http://code2.0beta.co.uk/moose/svn/Moose/trunk/t/200_examples/008_record_set_iterator.t
-
-This demonstration is not efficient since it seems that each 
-record object requires instantiation (costly).  
-
-
-=head2 Mixing of attributes, data and metadata
-
-It is bad design to store object attributes, data and metadata
-in the same construct, ie all as attributes.  A conflict arises when 
-the field names of data conflict with the field names of the metadata.
-These should be seperated.
-  
-The interface should be designed such that the user has
-access principally to the data, via object->field syntax.  And it seems
-sloppy to save the metadata with a alternate naming convention.  Some 
-alternative might be:
-
-A special slot for attributes and metadata.  These can be called 
-_data_ and _layout_, e.g.
-
-    $record->field_1
-
-    sub field1 :lvalue {
-
-            $_[0]->{_data_}->[ $_[0]->{_layout_}->{field1}->{pos} ]
-
-
-Use $record->meta already exists, so this seems like a bad place to store
-information.  
-
-Absent a better methodology we stick to _name_.  
-
+During object construction, name-based accessors are built for each 
+record field. By default, the accessors permit L-value assignment. 
 
 
 =head1 METHODS
@@ -285,38 +198,22 @@ Object constructor.  Creates and returns a ODG::Record object.  It
 takes the following options. 
 
 
-=over 4
-
-_layout_  A ODG::Layout object containing the metadata for 
-          record.  By convention, the first position has 
-          an index of 0.
-
-=back
-
-=head2 _data_ 
+=head2 _data 
 
 L-value object accessor to the record data.  Data is stroed internally
 as an array reference, so data  This is the very fast 
-accessor for the _data_,    
+accessor for the _data,    
 
   #  Getter
-    $record->_data_               # Retrieve entire array ref
-    $record->_data_->[ $index ]   # Get a specific field
+    $record->_data               # Retrieve entire array ref
+    $record->_data->[ $index ]   # Get a specific field
   
   # Setter
-    $record->_data_( [ .. ] )
-    $record->_data_ = [ .. ]
+    $record->_data( [ .. ] )
+    $record->_data = [ .. ]
 
-    $record->_data_->[ $index ] = $value    
+    $record->_data->[ $index ] = $value    
 
-
-=head2 _layout_
-
-READ-ONLY accessor to the layout object.  
-
-=head2 _metadata_
-
-Convenience method for accessing the _layout_->_metadata_ object.
 
 
 =head2 EXPORT
@@ -326,7 +223,9 @@ None by default.
 
 =head1 SEE ALSO
 
-ODG::Metadata, Moose
+L<MooseX::Meta::Attribute::Index>
+
+L<Moose>
 
 
 =head1 THANKS
@@ -336,6 +235,7 @@ Steven Little, author of L<Moose>
 Paul Driver for suggesting to place the accessor methods in the instance rather than the class.
 
 Members of moose@perl.org.
+
 
 =head1 AUTHOR
 
--- t/ODG-Record.t
+++ t/ODG-Record.t
@@ -1,120 +1,67 @@
 # Before `make install' is performed this script should be runnable with
 # `make test'. After `make install' it should work as `perl ODG-Record.t'
 
-#########################
-# change 'tests => 1' to 'tests => last_test_to_print';
 
-use Test::More tests => 17;
-use Benchmark qw( :all :hireswallclock );
-use Data::Dumper;
+use Test::More tests => 10;
 
-BEGIN { print "\nSection 1: Using Mods\n" };
 BEGIN { use_ok('ODG::Record') };
-BEGIN { use_ok('ODG::Layout') };
-BEGIN { use_ok('ODG::Metadata') };
 
 
-#########################
-print "\nSection 2: ODG::Metadata\n";
+{ 
+	package  # HIDE FROM PAUSE 
+		MyClass;
 
-  # Test ODG::Metadata::Field
-	my $metadata_1 = ODG::Metadata->new( { 
-	   name => 'first_name' ,
-	} );
+		use Moose;
+			extends 'ODG::Record';
 
-	my $metadata_2 = ODG::Metadata->new( { 
-	   name => 'last_name' ,
-	} );
+		has area_code => (
+			is 			=> 'ro' ,
+			isa			=> 'Int',
+			index		=> 0 ,
+			traits		=> [ 'Index' ] ,
+		);
 
-	my $metadata_3 = ODG::Metadata->new( { 
-	   name => 'daughter' ,
-	} );
+		has exchange	=> (
+			is			=> 'rw' ,
+			isa			=> 'Int' ,
+			index		=> 1 ,
+			traits		=> [ 'Index' ] ,
 
-	
-	isa_ok( $metadata_1, "ODG::Metadata"  );
+		);
 
-    can_ok( $metadata_1, qw( name ) );
-
-    ok( $metadata_1->name   eq 'first_name', 'Accessing via accessor' );
-    ok( $metadata_1->{name} eq 'first_name', 'Accessing directly directly' );
-
-    # is_faster( -5, sub { $metadata_1->name; }, sub { $metadata_1->{name}; } );
-    print "\n";
-    cmpthese( -0.2 , 
-             { 
-               'Accessor access'   =>  sub { $metadata_1->name }     ,
-               'Direct access'     =>  sub { $metadata_1->{name} }   ,
-             }    
-    );
+		has number	=> (
+			is			=> 'rw' ,
+			isa 		=> 'Int' ,
+			index		=> 2 ,
+			traits		=> [ 'Index' ] ,
+		);
 
 
-#########################
-print "\nSection 3: ODG::Layout Role\n";
-
-BEGIN {
-    package ODG::Layout::Blank;
-    use Moose;
-    with 'ODG::Layout';
 }
 
-    my $layout = ODG::Layout::Blank->new( { 
-            _metadata_ => 
-            [ 
-                $metadata_1 ,
-                $metadata_2 ,
-            ]
-    } );
 
-	isa_ok( $layout, 'ODG::Layout::Blank' ); 
-    can_ok( $layout, 
-            qw( 
-                first_name last_name 
-                _metadata_ _index_name
-                get set push pop shift unshift insert
-                clear delete empty
-                map find grep
-            ) );
-            
-    
-    ok( $layout->_metadata_->[1]->{index} == 1, 
-        "Metadata index" 
-    );  
+my $record = MyClass->new( _data => [ 213, 555, 1212 ] );
+	isa_ok( $record, 'MyClass' );
+	ok $record->_has_data, '_has_data';
+	ok $record->area_code == 213, 'ro attribute';
+	ok $record->exchange  == 555, 'rw attribute';
+	ok $record->number    == 1212,'rw attribute';
+
+
+	$record->exchange( 444 );
+	ok $record->exchange  == 444, 'Standard Moose Accessor no type checking';
+
+ 	$record->number = 1234;
+	ok $record->number    == 1234, 'Lvalue accessor';
+
+  # Data operations
+
+	ok $record->_has_data, '_has_data';
+	$record->_data = [ 000, 000, 0000 ];
+	ok $record->area_code == 000  , 'lvalue data replacement';
+
 	
-    ok( $layout->get(1)->name eq 'last_name' ,
-        "MooseX::AttributeHelper access"
-    );
 
-    $layout->push( $metadata_3 );
-    ok( $layout->get(2)->name eq 'daughter' ,
-        "Push metadata field"
-    );
-    
-    # print Dumper $layout;
-    # die;
 
-###########################    
-print "\nSection 4: ODG::Record\n";
 
-  # Test ODG::Record
-	my $record = ODG::Record->new( 
-	    { 
-	        _data_     => [ qw( fred flintstone pebbles) ] ,
-	        _layout_   => $layout 
-	    } 
-    );
-	
-	isa_ok( $record, 'ODG::Record' );
-    can_ok( $record, qw( first_name last_name daughter ) ); 
-
-###########################    
-print "\nSection 5: ODG::Record Setting and Getting\n";
-
-# Test Accessors
-ok( $record->last_name eq 'flintstone', 'Checking getter method' );
-
-$record->last_name = 'rubble';    
-ok( $record->last_name eq 'rubble', 'Checking lvalue setter method' );
-
-$record->last_name( 'flintstone' );
-ok( $record->last_name eq 'flintstone', 'Checking moose setter method' );
 
